3e3cfd8db134f6768ea9136a3e6fa3bad3eff61a
gizem <gizembolayir@gmail.com>
2025-07-24 16:38:26 +0300

    task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+
+button.addEventListener("click", () => {
+  const newColor = getRandomHexColor(); // Yeni renk oluştur
+  body.style.backgroundColor = newColor; // Body'nin arka planını değiştir
+  span.textContent = newColor; // Renk kodunu span içine yaz
+});
