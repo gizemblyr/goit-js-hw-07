7b7765653c6693bb3ce1f92d6546d508a50a93cc
gizem <gizembolayir@gmail.com>
2025-07-24 20:31:34 +0300

    changes

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,10 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// Elementleri oluştur
 const controls = document.createElement("div");
 controls.id = "controls";
 
@@ -18,7 +25,43 @@ destroyBtn.setAttribute("data-destroy", "");
 const boxes = document.createElement("div");
 boxes.id = "boxes";
 
-// Hepsini body'ye ekle
+// DOM'a ekle
 controls.append(input, createBtn, destroyBtn);
 document.body.append(controls, boxes);
 
+// Fonksiyonlar
+function createBoxes(amount) {
+  destroyBoxes(); // öncekileri temizle
+
+  const fragment = document.createDocumentFragment();
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+    size += 10;
+  }
+
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+// Olaylar
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+  input.value = "";
+});
